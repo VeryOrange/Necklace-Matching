76ac90bf47719d34590f2f98f21adab844b3ef3d
eric <hex@kali.kali>
2020-05-02 19:08:36 -0400

    Added bonus 1

--- necklace-matching.py
+++ necklace-matching.py
@@ -7,21 +7,35 @@ def same_necklace(a,b):
     word = a
     for i in range(len(a)):
         word = word[1:]+word[0]
-        print(word)
         if word == b:
             return True
     return False
+
+def bonus1(a):
+    if a == "":
+        return 1
+    word = a
+    count = 0 
+    for i in range(len(a)):
+        if word == a:
+            count += 1
+        word = word[1:]+word[0]
+    return count
 
 def main():
     print("Please give me two strings. I will let you know if they belong to the same necklace.")
     a = input()
     b = input()
 
+    print("Now give me another string. I will tell you how many times that string can repeat itself if scrambled")
+    c = input()
+
     ret = same_necklace(a,b)
+    count = bonus1(c)
 
     if(ret):
         print("These strings belong to the same necklace")
     else:
         print("These strings do not belong to the same necklace.")
-
+    print("The first string repeats " + str(count))
 main()
